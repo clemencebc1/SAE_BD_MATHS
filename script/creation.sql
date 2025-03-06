5c466b394ee77f732268a7497dc63598f919a558
Clemence <clemence.bcq@gmail.com~>
2025-03-06 12:41:57 +0100

    script relaionnel

--- script/creation.sql
+++ script/creation.sql
@@ -1,26 +1,26 @@
 -- create DATABASE PRODUCTION;
 
-create or replace type Details as object (
-    idComposant number(10),
-    quantite number(7),
-    member function get_idComp return number,
-    member function get_quantite return number
-);
-/
-create or replace type BODY Details AS
-    member function get_idComp RETURN number IS
-    BEGIN
-    return SELF.idComposant;
-    END get_idComp;
+-- create or replace type Details as object (
+--     idComposant number(10),
+--     quantite number(7),
+--     member function get_idComp return number,
+--     member function get_quantite return number
+-- );
+-- /
+-- create or replace type BODY Details AS
+--     member function get_idComp RETURN number IS
+--     BEGIN
+--     return SELF.idComposant;
+--     END get_idComp;
 
-    member function get_quantite RETURN number IS
-    BEGIN
-    return SELF.quantite;
-    END;
-END;
-/
-CREATE TYPE DetailsList AS VARRAY(200) OF Details;
-/
+--     member function get_quantite RETURN number IS
+--     BEGIN
+--     return SELF.quantite;
+--     END;
+-- END;
+-- /
+-- CREATE TYPE DetailsList AS VARRAY(200) OF Details;
+-- /
 CREATE TABLE PIECE (
     idPiece number(10) not null,
     nomPiece Varchar2(50),
@@ -35,10 +35,25 @@ CREATE TABLE PIECE_SIMPLE(
 
 CREATE TABLE PIECE_COMPOSEE (
     idPieceComposee number(10) not null,
-    detailsPiece DetailsList,
     constraint PKidPieceComp PRIMARY KEY (idPieceComposee)
 );
+CREATE TABLE Composant (
+    idComposant number(10) not null,
+    constraint PKidComposant PRIMARY KEY (idComposant)
+);
 
+CREATE TABLE Details (
+    idDetailsPiece number(10),
+    idDetailsComp number(10),
+    quantite number(7),
+    constraint PkidDetailsPiece PRIMARY KEY (idDetailsPiece, idDetailsComp)
+);
 ALTER TABLE PIECE_SIMPLE ADD CONSTRAINT FKidPieceSimp FOREIGN KEY (idPieceSimple) REFERENCES PIECE(idPiece);
 
 ALTER TABLE PIECE_COMPOSEE ADD CONSTRAINT FKidPieceComp FOREIGN KEY (idPieceComposee) REFERENCES PIECE(idPiece);
+
+ALTER TABLE Composant ADD CONSTRAINT FKidComp FOREIGN KEY (idComposant) REFERENCES PIECE(idPiece);
+
+ALTER TABLE Details ADD CONSTRAINT FKidCompDetails FOREIGN KEY (idDetailsComp) REFERENCES Composant(idComposant);
+
+ALTER TABLE Details ADD CONSTRAINT FKidDetailsPiece FOREIGN KEY (idDetailsPiece) REFERENCES PIECE(idPiece);
---
+++ script/insertions.sql
@@ -0,0 +1,3 @@
+insert into PIECE values (1, 'Vis', 'en titane');
+insert into PIECE_SIMPLE values (1);
+insert into 
